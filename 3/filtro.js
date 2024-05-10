860de0fd851a01001b190cab107e8dd4bafac2a2
Patopm <egs.pato.pm@gmail.com>
2024-05-10 13:34:14 -0600

    Se agregan los ejercicios a trabajar

---
+++ 3/filtro.js
@@ -0,0 +1,80 @@
+// Tenemos un li de productos
+
+const productos = [
+  {
+    nombre: "Zapato negro",
+    tipo: "zapato",
+    color: "negro",
+    img: "./taco-negro.jpg",
+  },
+  {
+    nombre: "Zapato azul",
+    tipo: "zapato",
+    color: "azul",
+    img: "./taco-azul.jpg",
+  },
+  {
+    nombre: "Bota negra",
+    tipo: "bota",
+    color: "negro",
+    img: "./bota-negra.jpg",
+  },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  {
+    nombre: "Zapato rojo",
+    tipo: "zapato",
+    color: "rojo",
+    img: "./zapato-rojo.jpg",
+  },
+];
+
+const li = document.getElementById("lista-de-productos");
+
+for (let i = 0; i < productos.length; i++) {
+  var d = document.createElement("div");
+  d.classList.add("producto");
+
+  var ti = document.createElement("p");
+  ti.classList.add("titulo");
+  ti.textContent = productos[i].nombre;
+
+  var imagen = document.createElement("img");
+  imagen.setAttribute("src", productos[i].img);
+
+  d.appendChild(ti);
+  d.appendChild(imagen);
+
+  li.appendChild(d);
+}
+
+const botonDeFiltro = document.querySelector("button");
+const $i = document.querySelector("input");
+botonDeFiltro.onclick = function () {
+  const texto = $i.value;
+  console.log(texto);
+  const productosFiltrados = filtrado(productos, texto);
+
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
+
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productosFiltrados[i].nombre;
+
+    var imagen = document.createElement("img");
+    imagen.setAttribute("src", productosFiltrados[i].img);
+
+    d.appendChild(ti);
+    d.appendChild(imagen);
+
+    li.appendChild(d);
+  }
+};
+
+const filtrado = (productos = [], texto) => {
+  texto.toLowerCase();
+  return productos.filter(
+    (item) => item.tipo.includes(texto) || item.color.includes(texto)
+  );
+};
