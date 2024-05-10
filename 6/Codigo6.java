deac18f4b02a3740a668eb5590b69b11901c37c2
Patopm <egs.pato.pm@gmail.com>
2024-05-10 14:20:51 -0600

    Codigo 6 completado

--- 6/Codigo6.java
+++ 6/Codigo6.java
@@ -1,24 +1,27 @@
-public class codigo6 {
-	  
-	    int[] n = int[20];
+import java.util.ArrayList;
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
-	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
+public class Codigo6 {
+	public static void main(String[] args) { // faltaba el metodo main
+		ArrayList<Integer> n = new ArrayList<Integer>();
 
-	    int multiplo = (opcion == 1) : 5 ? 7;
+		for (int i = 0; i < 20; i++) {
+			n.add((int) (Math.random() * 381) + 20);
+			System.out.print(n.get(i) + " ");
+		}
 
-	    foreach (char e : n) {
-	      if (e % multiplo == 0) {
-	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
-	      }
-	    }
-	  
+		System.out.println("\n¿Qué números quiere resaltar? ");
+		System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+		int opcion = Integer.parseInt(System.console().readLine());
+
+		int multiplo = (opcion == 1) ? 5 : 7; // se acomoda bien el operador terniario
+
+		for (Integer e : n) {
+			if (e % multiplo == 0) {
+				System.out.print("[" + e + "] ");
+			} else {
+				System.out.print(e + " ");
+			}
+		}
+
 	}
+}
